037182bb465e4064cd478a96c8960e482417ba24
2Machiavelli <stas.chetyrbok@mail.ru>
2021-09-08 15:55:10 +0300

    STYLE: update eslint, spases to tabs at jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,34 @@
 module.exports = {
-  testEnvironment: "jsdom",
-  verbose: true,
-  collectCoverage: true,
-  coverageThreshold: {
-    global: {
-      statement: 60
-    }
-  },
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js"
-  ],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-  },
-  modulePaths: [
-    "<rootDir>/src",
-    "<rootDir>/node_modules"
-  ],
-  transform: {
-    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
-    ".*\\.(js)$": "babel-jest",
-    ".*\\.(ts)$": "ts-jest",
-    ".*\\.(tsx)$": "ts-jest",
-  },
-  "globals": {
-    "react-jest": {
-      "tsConfigFile": "tsconfig.json"
-    }
-  },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(tsx?)$"
+	testEnvironment: "jsdom",
+	verbose: true,
+	collectCoverage: true,
+	coverageThreshold: {
+		global: {
+			statement: 60
+		}
+	},
+	moduleFileExtensions: [
+		"ts",
+		"tsx",
+		"js"
+	],
+	moduleNameMapper: {
+		"^@/(.*)$": "<rootDir>/src/$1",
+	},
+	modulePaths: [
+		"<rootDir>/src",
+		"<rootDir>/node_modules"
+	],
+	transform: {
+		".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
+		".*\\.(js)$": "babel-jest",
+		".*\\.(ts)$": "ts-jest",
+		".*\\.(tsx)$": "ts-jest",
+	},
+	"globals": {
+		"react-jest": {
+			"tsConfigFile": "tsconfig.json"
+		}
+	},
+	testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(tsx?)$"
 }
